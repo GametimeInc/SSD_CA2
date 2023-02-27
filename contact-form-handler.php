7c4e940872f4808088908dc92f9cfe944515dd20
Cian <cianashby1337@gmail.com>
2023-02-27 13:48:34 +0000

    Contact form fields updated and sendable

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -2,8 +2,30 @@
 
 $errors = '';
 $myemail = 'D00241714@student.dkit.ie';// <-----Put your DkIT email address here.
+
+/*
+  Form fields:
+  Your Name* = name
+  Country Code* = countryCode
+  Phone Number* = number
+  Email Address* = email
+  Reason for contact (Lost Animal/Found Animal)* = reason
+  Type of Animal* = typeOfAnimal
+  Pet Name = petName
+  Breed = breed
+  Picture = image
+  Message* = message
+*/
+
 if(empty($_POST['name'])  ||
+   empty($_POST['countryCode']) ||
+   empty($_POST['number'])  ||
    empty($_POST['email']) ||
+   empty($_POST['reason'])  ||
+   empty($_POST['typeOfAnimal']) ||
+   empty($_POST['petName'])  ||
+   empty($_POST['breed']) ||
+   empty($_POST['image'])  ||
    empty($_POST['message']))
 {
     $errors .= "\n Error: all fields are required";
@@ -16,7 +38,14 @@ $headers .= 'From: '.$myemail."\r\n".
 
 
 $name = $_POST['name'];
+$country_code = $_POST['countryCode'];
+$number = $_POST['number'];
 $email_address = $_POST['email'];
+$reason = $_POST['reason'];
+$type_of_animal = $_POST['typeOfAnimal'];
+$pet_name = $_POST['petName'];
+$breed = $_POST['breed'];
+$image = $_POST['image'];
 $message = $_POST['message'];
 
 if (!preg_match(
@@ -26,12 +55,14 @@ $email_address))
     $errors .= "\n Error: Invalid email address";
 }
 
+/*Remember to also upload datetime to database
+try console logging to check format of input datetime*/
 if( empty($errors))
 {
         $to = $myemail;
         $email_subject = "Contact form submission: $name";
         $email_body = "You have received a new message. ".
-        " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message";
+        " Here are the details:\n Name: $name \n countryCode: $country_code \n Number: $number \n Email: $email_address \n Reason: $reason \n Type of Animal: $type_of_animal \n Pet's Name: $pet_name \n Breed: $breed \n Image: $image \n Message: \n $message";
 
         mail($to,$email_subject,$email_body,$headers);
         //redirect to the 'thank you' page
--- contact-page.php
+++ contact-page.php
@@ -4,19 +4,77 @@
   <h1>Contact us</h1>
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
 <form method="POST" name="contactform" action="contact-form-handler.php"> 
+
+<!--
+  Form fields:
+  Your Name* = name
+  Country Code* = countryCode
+  Phone Number* = number
+  Email Address* = email
+  Reason for contact (Lost Animal/Found Animal)* = reason
+  Type of Animal* = typeOfAnimal
+  Pet Name = petName
+  Breed = breed
+  Picture = image
+  Message* = message
+-->
+
 <p>
 <label for='name'>Your Name:</label> <br>
-<input type="text" name="name">
+<input type="text" name="name" required>
 </p>
+
+<p>
+<label for='countryCode'>Phone Number:</label> 
+<select name="countryCode" required>
+  <option></option>
+  <option value="roi">+353</option>
+  <option value="ni">+44</option>
+</select>
+<input type="text" name="number"> <br>
+</p>
+
 <p>
 <label for='email'>Email Address:</label> <br>
 <input type="text" name="email"> <br>
 </p>
+
+<p>
+<label for='reason'>Reason for contact:</label> <br>
+<select name="reason">
+  <option default value="none"></option>
+  <option value="lostPet">Lost Pet</option>
+  <option value="foundPet">Found Pet</option>
+</select> <br>
+</p>
+
+<p>
+<label for='typeOfAnimal'>Type Of Animal:</label> <br>
+<input type="text" name="typeOfAnimal">
+</p>
+
+<p>
+<label for='petName'>Pet's Name:</label> <br>
+<input type="text" name="petName">
+</p>
+
+<p>
+<label for='breed'>Pet's Breed:</label> <br>
+<input type="text" name="breed">
+</p>
+
+<p>
+<label for='image'>Picture of Pet:</label> <br>
+<input type="file" name="image" accept="image/*">
+</p>
+
 <p>
 <label for='message'>Message:</label> <br>
 <textarea name="message"></textarea>
 </p>
+
 <input type="submit" value="Submit"><br>
+
 </form>
 
 </main><!-- /.container -->
