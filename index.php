eb075a321ed93f09c43cd5418856aee28624919f
Cian <cianashby1337@gmail.com>
2023-03-06 14:00:51 +0000

    Also added title mt to index page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <main class="container">
   <div class="starter-template text-center">
-    <h1>Pet lost it's way? We can help!</h1>
+    <h1 class="mt-4">Pet lost it's way? We can help!</h1>
     <p class="lead">We can be of service in either:</p>
     <ul id="indexButtons">
       <li><a class="nav-link btn btn-dark" aria-current="page" href="lost-pets.php">Finding your missing companion</a></li>
